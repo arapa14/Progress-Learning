090c1b9741972e7e471c4823bc20842edcbe6db1
arapa14 <arapa00016@gmail.com>
2025-01-01 12:36:40 +0700

    React Fund

--- REACT/tutorial-react/src/App.tsx
+++ REACT/tutorial-react/src/App.tsx
@@ -1,15 +1,14 @@
-import ListGroup from './components/ListGroup';
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
-  const items = [
-    "Jakarta",
-    "Tanggerang",
-    "Padang",
-    "Bandung",
-    "Bogor"
-  ];
+  const [showAlert, setShowAlert] = useState(false);
 
-  return <div><ListGroup items={items} heading={"Cities"}/></div>
-}
+  return <div>
+    {showAlert && <Alert onClose={() => setShowAlert(false)}>My Alert</Alert>}
+    <Button color="primary" onClick={() => setShowAlert(true)}>My Button</Button>
+  </div>
+} 
 
 export default App;
--- REACT/tutorial-react/src/components/ListGroup.tsx
+++ REACT/tutorial-react/src/components/ListGroup.tsx
@@ -5,10 +5,11 @@ import { useState } from "react";
 interface Props {
     items: string[];
     heading: string;
+    onSelectItem: (item: string) => void;
 }
-function ListGroup({items, heading}: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
     // Hook
-    const [selectedIndex, setSelectedIndex] =useState(-1);
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
     // const message = items.length === 0 ? <p>No item found</p> : null
 
@@ -25,8 +26,8 @@ function ListGroup({items, heading}: Props) {
             {getMessage()}
             <ul className="list-group">
                 {items.map((item, index) =>
-                    <li className={ selectedIndex === index ? "list-group-item active" : "list-group-item"}
-                        key={item} onClick={() => {setSelectedIndex(index);}}>{item}</li>
+                    <li className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+                        key={item} onClick={() => { setSelectedIndex(index); onSelectItem(item) }}>{item}</li>
                 )}
             </ul>
         </>
